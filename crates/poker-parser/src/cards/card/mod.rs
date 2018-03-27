b1c29182d0ac8b8bc11af0bfa3eee2e23fb73acb
Fabio Elizandro <fabio@elizandro.com.br>
2018-03-27 22:50:07 +1100

    feat(poker-parser): integrate number and suit parser

--- crates/poker-parser/src/cards/card/mod.rs
+++ crates/poker-parser/src/cards/card/mod.rs
@@ -1,2 +1,27 @@
 mod suit;
 mod number;
+
+#[derive(Debug, PartialEq)]
+pub struct Card {
+    number: u8,
+    suit: suit::Suit
+}
+
+pub fn parse(card: &str) -> Card {
+    Card {
+        number: number::parse(card),
+        suit: suit::parse(card)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_parsers_string_to_card() {
+        assert_eq!(parse("2H"), Card { number: 2, suit: suit::Suit::Hearts });
+        assert_eq!(parse("7S"), Card { number: 7, suit: suit::Suit::Spades });
+        assert_eq!(parse("AD"), Card { number: 14, suit: suit::Suit::Dimonds });
+    }
+}
