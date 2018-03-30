835c829d1b2c666b3e9d7f62ee194fd312b1d55f
Fabio Elizandro <fabio@elizandro.com.br>
2018-03-30 15:13:18 +1100

    chore(main): print poker-parser results

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 extern crate poker_parser;
 
 fn main() {
-    poker_parser::hi();
+    let input = "
+    Player1: 2H 3D 5S 9C KD  Player2: 2C 3H 4S 8C AH
+    Player1: 2H 3D 5S 9C AD  Player2: 2C 3H 4S 8C KH
+    ";
+
+    println!("{:?}", poker_parser::parse(input));
 }
